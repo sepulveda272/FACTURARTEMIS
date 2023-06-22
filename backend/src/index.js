fbc320f0daee0b857aa248ae04d67e1dcf3782f9
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2023-06-22 11:52:21 -0500

    feat: :tada: Que bendicion

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+import app from "./app.js";
+
+const main = () => {
+    app.listen(app.get('port'));
+    console.log(`por fin no toca estar tumbando el servidor ${app.get('port')}`);
+}
+
+main();
