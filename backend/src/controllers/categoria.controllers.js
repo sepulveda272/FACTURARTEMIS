5959e050a819f5fdd2b62fbf1ec4b708b78ccd3d
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2023-06-23 07:47:32 -0500

    feat: :memo: avance

--- backend/src/controllers/categoria.controllers.js
+++ backend/src/controllers/categoria.controllers.js
@@ -1,5 +1,10 @@
-const getCategorias = (req, res) => {
-    res.json({"Categoria": "Electrodomesticos"});
+import getConnection from "../db/database.js";
+
+const getCategorias = async (req, res) => {
+    const connection = await getConnection();
+    const result = await connection.query("SELECT CategoriaID,CategoriaNombre,Descripcion,Imagen FROM categorias");
+    console.log(result);
+    res.json(result)
 }
 
 export const methodsHTTP = {
