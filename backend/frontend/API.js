8e497bddde1cf3d1b75d5127bce6ec78428651dc
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2023-07-04 13:30:06 -0500

    feat: :sparkles: avances

--- backend/frontend/API.js
+++ backend/frontend/API.js
@@ -1,18 +1,38 @@
-const url = "http://localhost:4000/api/categorias/";
+const url = "http://localhost:4000/api/categorias";
 
 
 export const obtainCategories = async () => {
- 
+    try {
+        const categorias = await fetch(url);
+        const datoCategorias = await categorias.json();
+        return datoCategorias;
+    } catch (error) {
+        console.log(error,"No funshon :(");
+    }
 };
 
 
 export const nuevaCategoria = async (categoria) => {
-  
+    try {
+        await fetch(url,{
+            method: "POST",
+            body:JSON.stringify(categoria),
+            headers:{'Content-Type':'application/json'}
+        })
+    } catch (error) {
+        console.log(error,"No Funshion :(");
+    }
 };
 
 
 export const deleteCategory = async (id) => {
-  
+  try {
+        await fetch(`${url}/${id}`,{
+            method:'DELETE'
+        })
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 
@@ -24,9 +44,18 @@ export const obtenerCategory = async (id) => {
 
 
 export const editarCategory = async (category) => {
- 
+    try {
+        await fetch(`${urlCategorias}/${category.categoriaID}`, {
+            method: "PUT",
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify(datos)
+        }).then(response => response.json()).then(updatedDatos => {
+            console.log('Datos actualizados:', updatedDatos);
+        });
+    } catch (error) {
+      console.error('Error al actualizar los datos:', error);
+    }
 };
-
 
 
 
--- backend/frontend/index.js
+++ backend/frontend/index.js
@@ -1,15 +1,29 @@
-import {
- 
-
-} from "./API.js";
+import { obtainCategories } from "./API.js";
 
 document.addEventListener("DOMContentLoaded", () => {
- 
+    mostrarLista()
 });
 
 /* LISTAR CATEGORIAS  - CRUD (R) */
 
-
+async function mostrarLista(){
+    const categorias = await obtainCategories();
+    const tablaCategoria = document.querySelector("#categories");
+    categorias.forEach(element => {
+        const {CategoriaID,CategoriaNombre,Descripcion,Imagen} = element
+        tablaCategoria.innerHTML+=`
+        <tr>
+            <td>${CategoriaID}</td>
+            <td>${CategoriaNombre}</td>
+            <td>${Descripcion}</td>
+            <td>${Imagen}</td>
+            <th><button class="btn btn-success editar">EDITAR</button></th>
+            <th><button class="btn btn-danger delete">ELIMINAR</button></th>
+            <th><button class="btn btn-primary datalle">DETALLE</button></th>
+          </tr>
+        `
+    })
+}
 
 /* INGRESAR NUEVA CATEGORIA  - CRUD (C) */
 
